43dcce0c06d9167b1f73996a4890573fed5dfb77
a-nester <a_nester@ukr.net>
2024-03-10 18:54:20 +0200

    +

--- js/task-3.js
+++ js/task-3.js
@@ -1,21 +1,36 @@
-// Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input) підставляє його 
-// поточне значення в span#name-output як ім’я для привітання. Обов’язково очищай значення в інпуті 
-// по краях від пробілів . Якщо інпут порожній або містить лише пробіли, то замість імені у спан має 
-// підставлятися рядок "Anonymous".
+// Напиши скрипт, який під час набору тексту в інпуті input#name-input 
+//(подія input) підставляє його поточне значення в span#name-output 
+// як ім’я для привітання.Обов’язково очищай значення в інпуті 
+// по краях від пробілів . Якщо інпут порожній або містить лише пробіли, 
+// то замість імені у спан має підставлятися рядок "Anonymous".
 
 
-// <input type="text" id="name-input" placeholder="Please enter your name" />
+// <input type="text" id="name-input" placeholder="Please enter your name"/>
 // <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
 
 // На що буде звертати увагу ментор при перевірці:
 
 // На елементі input#name-input прослуховується подія input
-// Під час набору тексту в інпуті його поточне значення підставляється в span#name-output як ім’я для привітання
+// Під час набору тексту в інпуті його поточне значення підставляється 
+// в span#name - output як ім’я для привітання
 // Значення в інпуті очищене від пробілів по краях
-// Якщо інпут порожній або містить лише пробіли, то замість імені у спан має підставлятися рядок "Anonymous"
+// Якщо інпут порожній або містить лише пробіли, то замість імені у 
+// спан має підставлятися рядок "Anonymous"
 
-const handleClick = event => {
-    console.log(event);
-  };
-  
-  button.addEventListener("click", handleClick);
+const inputElem = document.querySelector("#name-input");
+const spanElem = document.querySelector("#name-output");
+
+inputElem.addEventListener("input", handleInput)
+
+function handleInput(event) {
+    console.log(event.target.value);
+    if (!event.target.value || event.target.value.includes(" ")) {
+        spanElem.textContent = "Anonymous";
+    }
+    else {
+        spanElem.textContent = inputElem.value.trim();
+    }
+}
+
+
+// console.dir(inputElem);
--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,13 @@
+// Напиши скрипт, який змінює колір фону елемента < body > через інлайн -
+  // стиль по кліку на button.change - color і задає це значення кольору 
+  // текстовим вмістом для span.color.
+
+<div class="widget">
+  <p>Background color: <span class="color">-</span></p>
+  <button type="button" class="change-color">Change color</button>
+</div>
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
